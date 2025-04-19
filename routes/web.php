1688d45e88b89e6ab81d8dbc8d4d89e4b81affb6
GaniViola <ganiprasetyo711.@gmail.com>
2025-04-19 13:17:32 +0700

    login url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/secure-area/login', [AuthController::class, 'login']);
